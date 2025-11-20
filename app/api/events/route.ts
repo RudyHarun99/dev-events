c72a401f435b0803d1b9f082f43c8559c5959c71
RudyHarun99 <rdyhrn@gmail.com>
2025-11-20 21:21:31 +0700

    progress 4: implement cloudinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
 import { Event } from "@/database";
 import connectToDatabase from "@/lib/mongodb";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest ) {
   try {
@@ -18,6 +20,30 @@ export async function POST(req: NextRequest ) {
       });
     };
 
+    const file = formData.get('image') as File;
+    if (!file) {
+      return NextResponse.json({
+        message: 'Image file is required',
+      }, {
+        status: 400,
+      });
+    };
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader.upload_stream({
+        resource_type: 'image',
+        folder: 'DevEvent',
+      }, (error, results) => {
+        if (error) reject(error);
+        resolve(results);
+      }).end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
+
     const createdEvent = await Event.create(event);
     return NextResponse.json({
       message: 'Event created Successfully',
@@ -29,6 +55,27 @@ export async function POST(req: NextRequest ) {
     console.error(e);
     return NextResponse.json({
       message: 'Event Creation Failed',
+      error: e instanceof Error ? e.message : 'Unknown',
+    }, {
+      status: 500,
+    });
+  };
+};
+
+export async function GET() {
+  try {
+    await connectToDatabase();
+    const events = await Event.find().sort({ createdAt: -1 });
+    return NextResponse.json({
+      message: 'Event Fetched Successfully',
+      events,
+    }, {
+      status: 200,
+    });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({
+      message: 'Event Fetching Failed',
       error: e instanceof Error ? e.message : 'Unknown',
     }, {
       status: 500,
