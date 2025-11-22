f76c05917fbcfb50d586f3890fed721e992cfe75
RudyHarun99 <rdyhrn@gmail.com>
2025-11-22 17:13:18 +0700

    fetch-api-route

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import {events} from "@/lib/constants";
+// import {events} from "@/lib/constants";
+import { IEvent } from "@/database";
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const Page = async () => {
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
+
     return (
         <section>
             <h1 className="text-center">
@@ -20,11 +25,12 @@ const Page = () => {
 
                 <ul className="events">
                     {
-                        events
-                            .map((event, idx) => (
-                                <li key={idx}>
-                                    <EventCard {...event} />
-                                </li>
+                        events &&
+                        events.length > 0 &&
+                        events.map((event: IEvent, idx: number) => (
+                            <li key={idx}>
+                                <EventCard {...event} />
+                            </li>
                         ))
                     }
                 </ul>
